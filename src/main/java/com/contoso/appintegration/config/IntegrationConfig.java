58c69f7689b736fcb85c2aee4fd29d4f59e34929
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-04-16 18:29:25 -0500

    Mensaje

--- src/main/java/com/contoso/appintegration/config/IntegrationConfig.java
+++ src/main/java/com/contoso/appintegration/config/IntegrationConfig.java
@@ -2,10 +2,13 @@ package com.contoso.appintegration.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.MessageChannels;
 import org.springframework.integration.http.dsl.Http;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.http.HttpMethod;
 
 @Configuration
@@ -14,10 +17,23 @@ public class IntegrationConfig {
 
     @Bean
     public IntegrationFlow httpFlow() {
-        return IntegrationFlows.from(Http.inboundGateway("/foo")
-        .requestMapping(m -> m.methods(HttpMethod.POST))
-        .requestPayloadType(String.class))
-        .channel("httpRequest")
-        .get();
+        return IntegrationFlows.from(Http.inboundGateway("/foo").
+                requestMapping(m -> m.methods(HttpMethod.POST))
+                .requestPayloadType(String.class)
+                .replyChannel(directChannel()))
+                .channel("httpRequest")
+                .get();
     }
+
+    @Bean
+    public MessageChannel directChannel() {
+        return MessageChannels.direct().get();
+    }
+
+    @ServiceActivator(inputChannel = "httpRequest")
+    public String upCase(String in) {
+        System.out.println("message received" + in);
+        return in.toUpperCase();
+    }
+
 }
